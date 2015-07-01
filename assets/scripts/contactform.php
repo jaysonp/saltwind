c5f89683589d6146764b1162252dc47e786a5a74
Jayson Powers <jayson.powers@gmail.com>
2015-07-01 17:16:52 -0400

    php & mySQL work

--- assets/scripts/contactform.php
+++ assets/scripts/contactform.php
@@ -1,5 +1,14 @@
 <?php
 
+$id = 1;
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+
+echo "name: $name<br />";
+echo "email: $email<br />";
+echo "comment: $comment<br />";
+
 //Connect to DB
 
 $servername = "localhost";
@@ -14,8 +23,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO salt_wind (name, email, Comment)
-VALUES ('$_POST[name]', '$_POST[email]', '$_POST[comment]')";
+$sql = "INSERT INTO saltwind (id, name, email, Comment)
+VALUES ($id, $name, $email, $comment)";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -25,7 +34,8 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 
-
+sleep(2);
+//echo "<meta http-equiv='refresh' content=\"0; url=http://www.convictioneliquid.info/conviction/form.php\">";
 
 
 ?>
